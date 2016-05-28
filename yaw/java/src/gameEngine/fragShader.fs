7b1b306ed8ee675cf142ffb1aabfe59676030280
clementber <clementd.bertrand@orange.fr>
2016-05-28 22:36:52 +0200

    Update fragShader.fs

--- yaw/java/src/gameEngine/fragShader.fs
+++ yaw/java/src/gameEngine/fragShader.fs
@@ -79,7 +79,7 @@ vec4 calcPointLight(PointLight light, vec3 position, vec3 normal)
     float distance = length(light_direction);
     float attenuationInv = light.att_constant + light.att_linear * distance +
         light.att_exponent * distance * distance;
-    if(attenuationInv){
+    if(attenuationInv==0){
         attenuationInv = 1;
     }
     return light_colour / attenuationInv;
