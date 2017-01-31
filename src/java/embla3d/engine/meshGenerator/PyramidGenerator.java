b25a3fe3dfa42ecd95634becf6d677147d59f810
Ismael hajy <ismael.hajy@gmail.com>
2017-01-31 01:19:45 +0100

    Update  documentation for the  skybox, mesh and meshGenerator package

--- src/java/embla3d/engine/meshGenerator/PyramidGenerator.java
+++ src/java/embla3d/engine/meshGenerator/PyramidGenerator.java
@@ -1,92 +1,115 @@
 package embla3d.engine.meshGenerator;
 
-import org.joml.Vector3f;
-
 import embla3d.engine.meshs.Material;
 import embla3d.engine.meshs.Mesh;
 import embla3d.engine.meshs.PyramidMesh;
+import org.joml.Vector3f;
 
 public class PyramidGenerator {
-    public static Mesh generate(float xLength, float yLength, float zLength, Material m){
-	float x = xLength/2f;
-	float y = yLength/2f;
-	float z = zLength/2f;
-	float[] vertices = new float[]{
-	    //Front face
-	    0, y, 0,
-	    x,  -y, z,
-	    -x,  -y, z,
-	    //Back face
-	    0, y, 0,
-	    -x, -y, -z,
-	    x, -y, -z,
-	    //Left face
-	    0, y, 0,
-	    -x, -y, z,
-	    -x, -y, -z,
-	    //Right face
-	    0, y, 0,
-	    x,  -y, -z,
-	    x, -y, z,
-	    //Bottom face
-	    x, -y, z,
-	    -x, -y,z,
-	    x, -y, -z,
-	    -x, -y, -z
-	};
-		
-	float frontNormal = (float) Math.sqrt(yLength * yLength + zLength * zLength);
-	if(frontNormal == 0)
-	    frontNormal = 0.00000000000001f;
-	float yFNormal = z/frontNormal;
-	float zNormal = y/frontNormal;
-		
-	float lateralNormal = (float) Math.sqrt(yLength * yLength + xLength * xLength);
-	if(lateralNormal == 0)
-	    lateralNormal = 0.00000000000001f;
-	float xNormal = y/lateralNormal;
-	float yLNormal = z/lateralNormal;
 
-	float[] normals = {
-	    //Front face
-	    0,yFNormal,zNormal,
-	    0,yFNormal,zNormal,
-	    0,yFNormal,zNormal,
-	    //Back face
-	    0,yFNormal,-zNormal,
-	    0,yFNormal,-zNormal,
-	    0,yFNormal,-zNormal,
-	    //Left face
-	    -xNormal,yLNormal,0,
-	    -xNormal,yLNormal,0,
-	    -xNormal,yLNormal,0,
-	    //Right face
-	    xNormal,yLNormal,0,
-	    xNormal,yLNormal,0,
-	    xNormal,yLNormal,0,
-	    //Bottom face
-	    0,-1f,0,
-	    0,-1f,0,
-	    0,-1f,0,
-	    0,-1f,0
-	};
-	
-	int[] indices = new int[]{
-	    //Front face
-	    0,2,1,
-	    //Back face
-	    3,5,4,
-	    //Left face
-	    6,8,7,
-	    //Right face
-	    9,11,10,
-	    //Bottom Face
-	    12,13,15,12,15,14
-	};
-			
-	return new PyramidMesh(vertices, m,normals, indices, xLength,yLength,zLength);
+    /**
+     * Generate a ground Mesh with the specified material , width, length and height.
+     * Create vertices and normals based on the specified width, length and height.
+     *
+     * @param xLength width
+     * @param yLength length
+     * @param zLength height
+     * @param m       material
+     * @return Mesh
+     */
+    public static Mesh generate(float xLength, float yLength, float zLength, Material m) {
+        float x = xLength / 2f;
+        float y = yLength / 2f;
+        float z = zLength / 2f;
+        float[] vertices = new float[]{
+                //Front face
+                0, y, 0,
+                x, -y, z,
+                -x, -y, z,
+                //Back face
+                0, y, 0,
+                -x, -y, -z,
+                x, -y, -z,
+                //Left face
+                0, y, 0,
+                -x, -y, z,
+                -x, -y, -z,
+                //Right face
+                0, y, 0,
+                x, -y, -z,
+                x, -y, z,
+                //Bottom face
+                x, -y, z,
+                -x, -y, z,
+                x, -y, -z,
+                -x, -y, -z
+        };
+
+        float frontNormal = (float) Math.sqrt(yLength * yLength + zLength * zLength);
+        if (frontNormal == 0)
+            frontNormal = 0.00000000000001f;
+        float yFNormal = z / frontNormal;
+        float zNormal = y / frontNormal;
+
+        float lateralNormal = (float) Math.sqrt(yLength * yLength + xLength * xLength);
+        if (lateralNormal == 0)
+            lateralNormal = 0.00000000000001f;
+        float xNormal = y / lateralNormal;
+        float yLNormal = z / lateralNormal;
+
+        float[] normals = {
+                //Front face
+                0, yFNormal, zNormal,
+                0, yFNormal, zNormal,
+                0, yFNormal, zNormal,
+                //Back face
+                0, yFNormal, -zNormal,
+                0, yFNormal, -zNormal,
+                0, yFNormal, -zNormal,
+                //Left face
+                -xNormal, yLNormal, 0,
+                -xNormal, yLNormal, 0,
+                -xNormal, yLNormal, 0,
+                //Right face
+                xNormal, yLNormal, 0,
+                xNormal, yLNormal, 0,
+                xNormal, yLNormal, 0,
+                //Bottom face
+                0, -1f, 0,
+                0, -1f, 0,
+                0, -1f, 0,
+                0, -1f, 0
+        };
+
+        int[] indices = new int[]{
+                //Front face
+                0, 2, 1,
+                //Back face
+                3, 5, 4,
+                //Left face
+                6, 8, 7,
+                //Right face
+                9, 11, 10,
+                //Bottom Face
+                12, 13, 15, 12, 15, 14
+        };
+
+        return new PyramidMesh(vertices, m, normals, indices, xLength, yLength, zLength);
     }
-    public static Mesh generate(float xLength, float yLength,float zLength, float cx, float cy, float cz, float r){
-	return generate(xLength, yLength,zLength, new Material(new Vector3f(cx, cy, cz), r));
+
+    /**
+     * Generate a pyramide mesh with the specified material (cx, cy, cz, reflectance), width, length and height
+     *
+     * @param xLength width
+     * @param yLength length
+     * @param zLength height
+     * @param cx      Red value of the material
+     * @param cy      Green  value of the material
+     * @param cz      Blue  value of the material
+     * @param r       reflectance
+     * @return Mesh
+     */
+    public static Mesh generate(float xLength, float yLength, float zLength, float cx, float cy, float cz, float r) {
+        return generate(xLength, yLength, zLength, new Material(new Vector3f(cx, cy, cz), r));
     }
 }
--- src/java/embla3d/engine/meshs/OctahedronMesh.java
+++ src/java/embla3d/engine/meshs/OctahedronMesh.java
@@ -1,11 +1,15 @@
 package embla3d.engine.meshs;
 
+/**
+ * Mesh representing a Octahedron : default weight 6
+ * Useless for the moment will be removed during the upcoming refactoring
+ */
 public class OctahedronMesh extends Mesh {
-    public OctahedronMesh(float[] vertices, Material material,float[] normals, int[] indices) {
-	super(vertices, material, normals, indices, 6);
+    public OctahedronMesh(float[] vertices, Material material, float[] normals, int[] indices) {
+        super(vertices, material, normals, indices, 6);
     }
-	
-    public OctahedronMesh(float[] vertices, float cx, float cy, float cz, float reflectance,float[] normals, int[] indices) {
-	super( vertices,  cx,  cy,  cz,  reflectance, normals,  indices,6);
+
+    public OctahedronMesh(float[] vertices, float cx, float cy, float cz, float reflectance, float[] normals, int[] indices) {
+        super(vertices, cx, cy, cz, reflectance, normals, indices, 6);
     }
 }
