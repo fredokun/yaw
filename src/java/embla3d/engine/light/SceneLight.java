0a419788da86d76281efb2e56c50f12118984101
Ismael hajy <ismael.hajy@gmail.com>
2017-02-14 08:50:52 +0100

    Change Constant naming

--- src/java/embla3d/engine/light/SceneLight.java
+++ src/java/embla3d/engine/light/SceneLight.java
@@ -1,38 +1,42 @@
 package embla3d.engine.light;
 
+import embla3d.engine.ShaderProgram;
 import org.joml.Matrix4f;
 import org.joml.Vector3f;
 import org.joml.Vector4f;
 
-import embla3d.engine.ShaderProgram;
-
 public class SceneLight {
     // Maximum number of pointLight and spotLight which can be created.
     // If this value is modified, it is needed to change the value in the frag.sh
-    public static final int maxPointlight = 5;
-    public static final int maxSpotlight = 5;
-	
+    public static final int MAX_POINTLIGHT = 5;
+    public static final int MAX_SPOTLIGHT = 5;
+    public float specularPower = 8;
     // Different lights used in the current scene
     private AmbientLight ambient;
     private DirectionalLight sun;
     private PointLight[] pointTable;
     private SpotLight[] spotTable;
-	
-    public float specularPower = 8;
 
     /**
      * Constructor without parameters, it used to create the maximum of point light and spot light.
-     *
      */
-    public SceneLight(){
-	removeAmbient();
+    public SceneLight() {
+        removeAmbient();
         removeSun();
-	pointTable = new PointLight [maxPointlight];
-	for(int i=0; i< maxPointlight;i++)
-	    pointTable[i] = new PointLight();
-	spotTable = new SpotLight [maxSpotlight];
-	for(int i=0; i< maxSpotlight;i++)
-	    spotTable[i] = new SpotLight();
+        pointTable = new PointLight[MAX_POINTLIGHT];
+        for (int i = 0; i < MAX_POINTLIGHT; i++)
+            pointTable[i] = new PointLight();
+        spotTable = new SpotLight[MAX_SPOTLIGHT];
+        for (int i = 0; i < MAX_SPOTLIGHT; i++)
+            spotTable[i] = new SpotLight();
+    }
+
+    public void removeAmbient() {
+        this.ambient = new AmbientLight();
+    }
+
+    public void removeSun() {
+        this.sun = new DirectionalLight();
     }
 
     /**
@@ -40,45 +44,24 @@ public class SceneLight {
      */
 
     public void setAmbient(AmbientLight ambient) {
-	this.ambient = ambient;
-    }
-
-    public void removeAmbient() {
-	this.ambient = new AmbientLight();
-    }
-
-    public void setSun(DirectionalLight sun) {
-	this.sun = sun;
+        this.ambient = ambient;
     }
 
     public DirectionalLight getSun() {
-	return this.sun;
+        return this.sun;
     }
 
-    public void removeSun() {
-	this.sun = new DirectionalLight();
+    public void setSun(DirectionalLight sun) {
+        this.sun = sun;
     }
 
-    /**
-     * setters
-     */
-
-    public void setPointTable(PointLight[] pointTable) {
-	this.pointTable = pointTable;
-    }
-
-    public void setSpotTable(SpotLight[] spotTable) {
-	this.spotTable = spotTable;
-    }
-	
     public void setPointTable(PointLight point, int pos) {
-	this.pointTable[pos] = point;
+        this.pointTable[pos] = point;
     }
 
     public void setSpotTable(SpotLight spot, int pos) {
-	this.spotTable[pos] = spot;
+        this.spotTable[pos] = spot;
     }
-
 
     /**
      * Set to the render the different light
@@ -87,12 +70,12 @@ public class SceneLight {
      * @param viewMatrix viewMatrix
      */
 
-    public void render(ShaderProgram sh,Matrix4f viewMatrix){
+    public void render(ShaderProgram sh, Matrix4f viewMatrix) {
         sh.setUniform("ambientLight", ambient);
         sh.setUniform("specularPower", specularPower);
 
         // Process Point Lights
-        for (int i = 0; i < maxPointlight; i++) {
+        for (int i = 0; i < MAX_POINTLIGHT; i++) {
             // Get a copy of the point light object and transform its position to view coordinates
             PointLight currPointLight = new PointLight(pointTable[i]);
             Vector3f lightPos = new Vector3f(currPointLight.getPosition());
@@ -105,12 +88,12 @@ public class SceneLight {
         }
 
         // Process Spot Ligths
-        for (int i = 0; i < maxSpotlight; i++) {
+        for (int i = 0; i < MAX_SPOTLIGHT; i++) {
             // Get a copy of the spot light object and transform its position and cone direction to view coordinates
             SpotLight currSpotLight = new SpotLight(spotTable[i]);
             Vector4f dir = new Vector4f(currSpotLight.conedir, 0);
             dir.mul(viewMatrix);
-            currSpotLight.conedir=new Vector3f(dir.x, dir.y, dir.z);
+            currSpotLight.conedir = new Vector3f(dir.x, dir.y, dir.z);
             currSpotLight.cutoffAngle = (float) Math.cos(Math.toRadians(currSpotLight.cutoffAngle));
             Vector3f lightPos = new Vector3f(currSpotLight.getPosition());
             Vector4f aux = new Vector4f(lightPos, 1);
@@ -126,7 +109,7 @@ public class SceneLight {
         DirectionalLight currDirLight = new DirectionalLight(sun);
         Vector4f dir = new Vector4f(currDirLight.direction, 0);
         dir.mul(viewMatrix);
-        currDirLight.direction=new Vector3f(dir.x, dir.y, dir.z);
+        currDirLight.direction = new Vector3f(dir.x, dir.y, dir.z);
         sh.setUniform("directionalLight", currDirLight);
     }
 
@@ -134,35 +117,47 @@ public class SceneLight {
      * getters and setters
      */
 
-    public void setPointLight(PointLight pl, int pos){
-	pointTable[pos]=pl;
+    public void setPointLight(PointLight pl, int pos) {
+        pointTable[pos] = pl;
     }
-	
-    public void setSpotLight(SpotLight sl, int pos){
-	spotTable[pos]=sl;
+
+    public void setSpotLight(SpotLight sl, int pos) {
+        spotTable[pos] = sl;
     }
 
     public AmbientLight getAmbientLight() {
-	return ambient;
+        return ambient;
     }
-    
+
     public int getMaxSpotLight() {
-	return maxPointlight;
+        return MAX_POINTLIGHT;
     }
-    
+
     public PointLight[] getPointTable() {
-	return pointTable;
+        return pointTable;
+    }
+
+    /**
+     * setters
+     */
+
+    public void setPointTable(PointLight[] pointTable) {
+        this.pointTable = pointTable;
     }
 
     public SpotLight[] getSpotTable() {
-	return spotTable;
+        return spotTable;
     }
-	
-    public float getSpecularPower(){
-	return specularPower;
+
+    public void setSpotTable(SpotLight[] spotTable) {
+        this.spotTable = spotTable;
     }
-	
-    public void setSpecularPower(float x){
-	specularPower=x;
+
+    public float getSpecularPower() {
+        return specularPower;
+    }
+
+    public void setSpecularPower(float x) {
+        specularPower = x;
     }
 }
--- src/java/embla3d/engine/light/SpotLight.java
+++ src/java/embla3d/engine/light/SpotLight.java
@@ -3,10 +3,10 @@ package embla3d.engine.light;
 import org.joml.Vector3f;
 
 /**
-* The spot light is a type of light models a light source that’s emitted from a point in space, but instead of emitting in all directions is restricted to a cone.
-*
-* This class allows to create the spotlight. So, we need the class PointLight and to add the parameters conedir, cutoffAngle to realize a cone.
-**/
+ * The spot light is a type of light models a light source that’s emitted from a point in space, but instead of emitting in all directions is restricted to a cone.
+ * <p>
+ * This class allows to create the spotlight. So, we need the class PointLight and to add the parameters conedir, cutoffAngle to realize a cone.
+ **/
 
 public class SpotLight extends PointLight {
     Vector3f conedir;
@@ -15,54 +15,54 @@ public class SpotLight extends PointLight {
     /**
      * Constructor with the specified color, position, intensity, constantAtt, linearAtt, quadraticAtt, conedir, cutoffAngle
      *
-     * @param color         color
-     * @param position      position
-     * @param intensity     intensity
-     * @param constantAtt   constantAtt
-     * @param linearAtt     linearAtt
-     * @param quadraticAtt  quadraticAtt
-     * @param conedir       direction conedir
-     * @param cutoffAngle   angle
+     * @param color        color
+     * @param position     position
+     * @param intensity    intensity
+     * @param constantAtt  constantAtt
+     * @param linearAtt    linearAtt
+     * @param quadraticAtt quadraticAtt
+     * @param conedir      direction conedir
+     * @param cutoffAngle  angle
      */
     public SpotLight(Vector3f color, Vector3f position, float intensity,
-		     float constantAtt, float linearAtt, float quadraticAtt, Vector3f conedir, float cutoffAngle) {
-	super(color,position,intensity,constantAtt,linearAtt,quadraticAtt);
-	this.conedir = conedir;
-	this.cutoffAngle = cutoffAngle;
+                     float constantAtt, float linearAtt, float quadraticAtt, Vector3f conedir, float cutoffAngle) {
+        super(color, position, intensity, constantAtt, linearAtt, quadraticAtt);
+        this.conedir = conedir;
+        this.cutoffAngle = cutoffAngle;
     }
 
     /**
-     *Constructor with the specified color (cx,cy,cz), position (px,py,pz), intensity, constantAtt, linearAtt, quadraticAtt, cone direction (cdx,cdy,cdz) and angle
+     * Constructor with the specified color (cx,cy,cz), position (px,py,pz), intensity, constantAtt, linearAtt, quadraticAtt, cone direction (cdx,cdy,cdz) and angle
      *
-     * @param cx            Red value
-     * @param cy            Green value
-     * @param cz            Blue value
-     * @param px            Position x
-     * @param py            Position y
-     * @param pz            Position z
-     * @param intensity     intensity
-     * @param constantAtt   constantAtt
-     * @param linearAtt     linearAtt
-     * @param quadraticAtt  quadraticAtt
-     * @param cdx           Direction cone x
-     * @param cdy           Direction cone y
-     * @param cdz           Direction cone z
-     * @param cutoffAngle   angle
+     * @param cx           Red value
+     * @param cy           Green value
+     * @param cz           Blue value
+     * @param px           Position x
+     * @param py           Position y
+     * @param pz           Position z
+     * @param intensity    intensity
+     * @param constantAtt  constantAtt
+     * @param linearAtt    linearAtt
+     * @param quadraticAtt quadraticAtt
+     * @param cdx          Direction cone x
+     * @param cdy          Direction cone y
+     * @param cdz          Direction cone z
+     * @param cutoffAngle  angle
      */
     public SpotLight(float cx, float cy, float cz, float px, float py, float pz, float intensity,
-		     float constantAtt, float linearAtt, float quadraticAtt, float cdx, float cdy, float cdz, float cutoffAngle) {
-	super(cx,cy,cz,px,py,pz,intensity,constantAtt,linearAtt,quadraticAtt);
-	this.conedir = new Vector3f(cdx, cdy, cdz);
-	this.cutoffAngle = cutoffAngle;
+                     float constantAtt, float linearAtt, float quadraticAtt, float cdx, float cdy, float cdz, float cutoffAngle) {
+        super(cx, cy, cz, px, py, pz, intensity, constantAtt, linearAtt, quadraticAtt);
+        this.conedir = new Vector3f(cdx, cdy, cdz);
+        this.cutoffAngle = cutoffAngle;
     }
 
     /**
      * Constructor without parameters
      */
-    public SpotLight(){
-    	super();
-    	conedir = new Vector3f(0,0,-1);
-    	cutoffAngle = 0;
+    public SpotLight() {
+        super();
+        conedir = new Vector3f(0, 0, -1);
+        cutoffAngle = 0;
     }
 
     /**
@@ -70,32 +70,32 @@ public class SpotLight extends PointLight {
      *
      * @param source source
      */
-    public SpotLight(SpotLight source){
-    	super(source);
-    	conedir = source.conedir;
-    	cutoffAngle = source.cutoffAngle;
+    public SpotLight(SpotLight source) {
+        super(source);
+        conedir = source.conedir;
+        cutoffAngle = source.cutoffAngle;
     }
 
     /**
      * getters and setters
      */
     public Vector3f getConedir() {
-	return conedir;
+        return conedir;
     }
-	
-    public void setConedir(float x,float y,float z){
-	this.conedir=new Vector3f(x,y,z);
+
+    public void setConeDir(float x, float y, float z) {
+        this.conedir = new Vector3f(x, y, z);
     }
-	
-    public void setConeDir(Vector3f cd){
-	this.conedir=cd;
+
+    public void setConeDir(Vector3f cd) {
+        this.conedir = cd;
     }
 
     public float getCutoffAngle() {
-	return cutoffAngle;
+        return cutoffAngle;
     }
-	
-    public void setCutoffAngle(float coa){
-	this.cutoffAngle=coa;
+
+    public void setCutoffAngle(float coa) {
+        this.cutoffAngle = coa;
     }
 }
