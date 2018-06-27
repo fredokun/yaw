a70384f7edb89eedf124e19840877d6aff4a6c37
Clément Busschaert <clement.busschaert@outlook.com>
2018-06-27 17:45:08 +0200

    spec cleanup (breaking)
    .
    
    
    .

--- src/clojure/yaw/util.clj
+++ src/clojure/yaw/util.clj
@@ -1,11 +1,9 @@
 (ns yaw.util)
 
-
 (defn explode
   "Explodes a vector [x y z] tuple into (:x x :y y :z z)"
   [[x y z]]
   (list :x x :y y :z z))
-
 
 (defn -?
   "Takes two nilable numbers,"
@@ -18,9 +16,7 @@
       a
       (- a b))))
 
-
-
-(def color-kw
+(def kw->rgb
   {:black [0 0 0]
    :red [1 0 0]
    :blue [0 0 1]
@@ -30,3 +26,10 @@
    :magenta [1 0 1]
    :white [1 1 1]})
 
+(defn vec-or
+  "If any values in the first vector argument is nil,
+  replace it by the value at the same index in the second argument"
+  [a b]
+  (if (not (coll? b))
+    a
+    (mapv #(or %1 %2) a b)))
